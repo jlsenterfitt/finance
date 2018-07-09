80f00b3e4ea984b3c544c457ffe14bebcf150385
jlsenterfitt <jlsenterfitt@gmail.com>
2018-07-09 17:52:40 -0400

    Write Portfolio._getBacktestedReturns

--- Portfolio.py
+++ Portfolio.py
@@ -1,16 +1,20 @@
+import numpy as np
+
+
 class Portfolio(object):
     """Stores information about a current, desired, or potential portfolio."""
 
-    def __init__(self, percent_allocations, stock_database):
+    def __init__(self, percent_allocations, stock_db):
         """Initialize the portfolio.
 
         Args:
             percent_allocations {dict}: Dictionary of tickers to percent
                 allocations.
-            stock_database {StockDatabase}: Database of necessary information.
+            stock_db {StockDatabase}: Database of necessary information.
         """
-        # TODO-implement
-        pass
+        self._stock_db = stock_db
+        self.percent_allocations = percent_allocations
+        self.backtested_returns = self._getBacktestedReturns()
 
     def _getBacktestedReturns(self):
         """Calculate backtested returns for the portfolio.
@@ -19,5 +23,15 @@ class Portfolio(object):
             backtested_returns {OrderedDict}: Dictionary of dates to returns,
                 with dates in order.
         """
-        # TODO-implement
-        pass
+        # Get allocations in same order as stock database.
+        allocation_array = []
+        for ticker in self._stock_db.tickers:
+            if ticker in self.percent_allocations:
+                allocation_array.append([self.percent_allocations[ticker]])
+            else:
+                allocation_array.append([0])
+        allocation_array = np.array(allocation_array, dtype=np.float64)
+
+        nested_returns = np.matmul(
+            self._stock_db.price_change_array, allocation_array)
+        return np.transpose(nested_returns)[0]
---
+++ test_Portfolio.py
@@ -0,0 +1,53 @@
+from collections import OrderedDict
+from decimal import Decimal
+import unittest
+
+import Config
+from Stock import Stock
+from StockDatabase import StockDatabase
+from Portfolio import Portfolio
+
+
+class Test_Portfolio(unittest.TestCase):
+
+    def setUp(self):
+        # Change the config value.
+        Config.MINIMUM_AMOUNT_DATA = 4
+
+        test_data = OrderedDict()
+        test_data['2018-01-01'] = Decimal('100.00')
+        test_data['2018-01-02'] = Decimal('102.00')
+        test_data['2018-01-03'] = Decimal('104.00')
+        test_data['2018-01-04'] = Decimal('108.00')
+        test_data['2018-01-05'] = Decimal('110.00')
+        stock1 = Stock(test_data, '1', 0)
+
+        test_data = OrderedDict()
+        test_data['2018-01-01'] = Decimal('200.00')
+        test_data['2018-01-02'] = Decimal('202.00')
+        test_data['2018-01-03'] = Decimal('206.00')
+        test_data['2018-01-04'] = Decimal('208.00')
+        test_data['2018-01-05'] = Decimal('210.00')
+        stock2 = Stock(test_data, '2', 0)
+
+        test_data = OrderedDict()
+        test_data['2018-01-01'] = Decimal('300.00')
+        test_data['2018-01-02'] = Decimal('309.00')
+        test_data['2018-01-03'] = Decimal('318.00')
+        test_data['2018-01-04'] = Decimal('327.00')
+        test_data['2018-01-05'] = Decimal('336.00')
+        stock3 = Stock(test_data, '3', 0)
+
+        test_stock_dict = {'1': stock1, '2': stock2, '3': stock3}
+
+        self.stock_db = StockDatabase(test_stock_dict)
+
+    def test_getBacktestedReturns(self):
+        percent_allocations = {'1': 0.5, '2': 0.4, '3': 0.1}
+        portfolio = Portfolio(percent_allocations, self.stock_db)
+        self.assertAlmostEqual(list(portfolio.backtested_returns)[0], 1.017)
+        self.assertEqual(len(portfolio.backtested_returns), 4)
+
+
+if __name__ == '__main__':
+    unittest.main()
